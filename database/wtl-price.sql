80aaf5d10a6aee7bfbe58116563084597cd03c23
kapil97 <kamleshwar.kamleshwar15@siesgst.ac.in>
2017-10-19 06:59:51 +0530

    added database

---
+++ database/wtl-price.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 19, 2017 at 03:24 AM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wtl-price`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `p_id` varchar(5) NOT NULL,
+  `P_name` varchar(50) DEFAULT NULL,
+  `p_ecw1` varchar(8) DEFAULT NULL,
+  `p_ecw2` varchar(8) DEFAULT NULL,
+  `p_ecw3` varchar(8) DEFAULT NULL,
+  `p_link` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userinfo`
+--
+
+CREATE TABLE `userinfo` (
+  `u_id` int(5) NOT NULL,
+  `u_name` varchar(20) DEFAULT NULL,
+  `u_mail` varchar(30) DEFAULT NULL,
+  `u_pass` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userinfo`
+--
+
+INSERT INTO `userinfo` (`u_id`, `u_name`, `u_mail`, `u_pass`) VALUES
+(1, 'test123', 'test1@gmail.com', 'test123'),
+(2, 'kapil', 'kapil123', '0514a615be29e74c01ff');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `webinfo`
+--
+
+CREATE TABLE `webinfo` (
+  `w_id` varchar(20) NOT NULL,
+  `w_name` varchar(20) DEFAULT NULL,
+  `w_link` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `webinfo`
+--
+
+INSERT INTO `webinfo` (`w_id`, `w_name`, `w_link`) VALUES
+('ecw1', 'flipkart', 'www.flipkart.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`p_id`);
+
+--
+-- Indexes for table `userinfo`
+--
+ALTER TABLE `userinfo`
+  ADD PRIMARY KEY (`u_id`);
+
+--
+-- Indexes for table `webinfo`
+--
+ALTER TABLE `webinfo`
+  ADD PRIMARY KEY (`w_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `userinfo`
+--
+ALTER TABLE `userinfo`
+  MODIFY `u_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
